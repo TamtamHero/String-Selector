82746a2a52dc9edea86f2ea2cf17ed4c170853a4
TamTamHero <10632523+TamtamHero@users.noreply.github.com>
2025-03-24 11:39:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# string-selector
+
+This extension allows to quickly select text within strings delimited by single or double quotes. Priority is given to double quotes.
+The selection is triggered by the shortcut `extension.selectTextInQuotes` which is by default `ctrl+q`.
+
---
+++ src/extension.ts
@@ -0,0 +1,55 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+    let disposable = vscode.commands.registerCommand('extension.selectTextInQuotes', () => {
+        const editor = vscode.window.activeTextEditor;
+
+        if (editor) {
+            const document = editor.document;
+            const cursorPosition = editor.selection.active;
+
+            // Get the current line text
+            const lineText = document.lineAt(cursorPosition.line).text;
+            const posLeftDoubleQuote = Math.max(lineText.lastIndexOf('"', cursorPosition.character - 1), 0);
+            const posRightDoubleQuote = lineText.indexOf('"', cursorPosition.character);
+            const posLeftSingleQuote = Math.max(lineText.lastIndexOf('\'', cursorPosition.character - 1), 0);
+            const posRightSingleQuote = lineText.indexOf('\'', cursorPosition.character);
+
+            let start =0, end = 0;
+
+            // if the cursor is between 2 double quotes
+            if (posLeftDoubleQuote >= 0 && posRightDoubleQuote > posLeftDoubleQuote) {
+                // if the cursor is also between 2 single quotes which are both closer to cursor, use them a selection delimiter
+                if ((posLeftSingleQuote >= 0 && posRightSingleQuote > posLeftSingleQuote) && (posLeftSingleQuote > posLeftDoubleQuote && posRightSingleQuote < posRightDoubleQuote)) {
+                    start = posLeftSingleQuote;
+                    end = posRightSingleQuote;
+                }
+                // else, use double quotes
+                else{
+                    start = posLeftDoubleQuote;
+                    end = posRightDoubleQuote;
+                }
+            }
+            // else, if the cursor is between 2 single quotes, use them as selection delimiter
+            else if(posLeftSingleQuote >= 0 && posRightSingleQuote > posLeftSingleQuote){
+                start = posLeftSingleQuote;
+                end = posRightSingleQuote;
+            }
+
+            // Check if start and end are valid
+            if (start !== 0 && end !== 0) {
+                const startPosition = new vscode.Position(cursorPosition.line, start + 1);
+                const endPosition = new vscode.Position(cursorPosition.line, end);
+                const selection = new vscode.Selection(startPosition, endPosition);
+                editor.selection = selection;
+            } else {
+                vscode.window.showInformationMessage("Cursor is not within quotes.");
+            }
+        }
+    });
+
+    context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
